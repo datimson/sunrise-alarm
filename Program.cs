034c3ade0f5499658cc452f7dde871fa6a6acb50
Dean <dean@refereum.com>
2018-12-22 13:43:56 +1000

    new console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sunrise_alarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
